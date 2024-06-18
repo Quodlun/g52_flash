80ed6bf675152805fc62042432531da3f440274b
Saiki <115924991+Quodlun@users.noreply.github.com>
2024-06-18 21:20:30 +0800

    Added Flash & 7 Secends Cooldown.

--- g52_flash.ino
+++ g52_flash.ino
@@ -1,31 +1,91 @@
 byte ledPins [] = { A0, A1, A2, A3, A4, A5 };
-int buttonPin = 5;
+byte segPins [] = { 13, 12, 11, 10, 9, 8, 7 };
+byte btnPins [] = { 5, 4, 3, 2 };
+int flashBtnPin = 6;
+int startTime;
+
+int brightness = 1023;
+
+byte segDigits [ 10 ] [ 7 ] = 
+{
+  { 1, 1, 1, 1, 1, 1, 0 },
+  { 0, 1, 1, 0, 0, 0, 0 },
+  { 1, 1, 0, 1, 1, 0, 1 },
+  { 1, 1, 1, 1, 0, 0, 1 },
+  { 0, 1, 1, 0, 0, 1, 1 },
+  { 1, 0, 1, 1, 0, 1, 1 },
+  { 1, 0, 1, 1, 1, 1, 1 },
+  { 1, 1, 1, 0, 0, 0, 0 },
+  { 1, 1, 1, 1, 1, 1, 1 },
+  { 1, 1, 1, 1, 0, 1, 1 }
+};
 
 void setup ()
 {
-  for ( int i = 0; i < 8; i ++ )
+  Serial.begin ( 9600 );
+
+  pinMode ( flashBtnPin, INPUT );
+
+  for ( int i = 0; i < 6; i ++ )
   {
     pinMode ( ledPins [ i ], OUTPUT );
-    digitalWrite ( ledPins [ i ], LOW );
   }
+
+  for ( int i = 0; i < 7; i ++ )
+  {
+    pinMode ( segPins [ i ], OUTPUT );
+  }
+
+  for ( int i = 0; i < 4; i ++ )
+  {
+    pinMode ( btnPins [ i ], INPUT );
+    digitalWrite ( btnPins [ i ], LOW );
+  }
+
+  startTime = -7000;
 }
 
 void loop ()
 {
-  if ( digitalRead ( buttonPin ) == HIGH )
+  if ( digitalRead ( flashBtnPin ) == HIGH )
+  {
+    flash ();
+  }
+  
+  for ( int i = 0; i < 4; i ++ )
+  {
+    if ( digitalRead ( btnPins [ i ] ) == HIGH )
+    {
+      Serial.println ( i + 1 );
+
+      segDisplay ( i + 1 );
+    }
+  }
+}
+
+void segDisplay ( int displayNum )
+{
+  for ( int i = 0; i < 7; i ++ )
+  {
+    digitalWrite ( segPins [ i ], segDigits [ displayNum ] [ i ] );
+  }
+}
+
+void flash ()
+{
+  if ( millis () - startTime >= 7000 )
   {
     for ( int i = 0; i < 6; i ++ )
     {
-      analogWrite ( ledPins [ i ], 1023 );
+      analogWrite ( ledPins [ i ], brightness );
     }
 
+    startTime = millis ();
     delay ( 200 );
 
     for ( int i = 0; i < 6; i ++ )
     {
       analogWrite ( ledPins [ i ], 0 );
     }
-
-    delay ( 7000 );
   }
 }
