6569cd8ce3abf8b6b0c4f1763c2490d19f0ff169
Saiki <115924991+Quodlun@users.noreply.github.com>
2024-06-18 22:16:53 +0800

    Added Button Functions.

--- g52_flash.ino
+++ g52_flash.ino
@@ -1,10 +1,14 @@
-byte ledPins [] = { A0, A1, A2, A3, A4, A5 };
+byte ledPins [] = { A0, A1, A2, A3, A4, A5 }; // A6, A7
 byte segPins [] = { 13, 12, 11, 10, 9, 8, 7 };
 byte btnPins [] = { 5, 4, 3, 2 };
+byte brightnessList [] = { 100, 200, 300, 400, 500, 600, 700, 800, 900, 1023 };
+byte delayTimeList [] = { 50, 100, 150, 200, 250, 300 };
+
 int flashBtnPin = 6;
 int startTime;
 
-int brightness = 1023;
+int brightnessStage = 9;
+int delayTimeStage = 3;
 
 byte segDigits [ 10 ] [ 7 ] = 
 {
@@ -56,9 +60,79 @@ void loop ()
   {
     if ( digitalRead ( btnPins [ i ] ) == HIGH )
     {
-      Serial.println ( i + 1 );
+      delay ( 20 );
 
-      segDisplay ( i + 1 );
+      switch ( btnPins [ i ] )
+      {
+        case 5:
+          brightnessStage = 9;
+          delayTimeStage = 3;
+
+          Serial.println ( "Reset..." );
+          Serial.println ( brightnessStage );
+          Serial.println ( delayTimeStage );
+
+          break;
+
+        case 4:
+          delayTimeStage += 1;
+
+          if ( delayTimeStage > 5 )
+          {
+            delayTimeStage = 0;
+            segDisplay ( delayTimeStage );
+            Serial.println ( delayTimeStage );
+          }
+
+          else
+          {
+            segDisplay ( delayTimeStage );
+            Serial.println ( delayTimeStage );
+          }
+
+          break;
+          
+        case 3:
+          brightnessStage += 1;
+
+          if ( brightnessStage > 9)
+          {
+            brightnessStage = 9;
+            segDisplay ( brightnessStage );
+            Serial.println ( brightnessStage );
+          }
+
+          else
+          {
+            segDisplay ( brightnessStage );
+            Serial.println ( brightnessStage );
+          }
+
+          break;
+
+        case 2:
+          brightnessStage -= 1;
+
+          if ( brightnessStage < 0 )
+          {
+            brightnessStage = 0;
+            segDisplay ( brightnessStage );
+            Serial.println ( brightnessStage );
+          }
+
+          else
+          {
+            segDisplay ( brightnessStage );
+            Serial.println ( brightnessStage );
+          }
+
+          break;
+
+        default:
+          Serial.println ( btnPins [ i ] + 1 );
+
+          break;
+      }
     }
   }
 }
@@ -77,11 +151,11 @@ void flash ()
   {
     for ( int i = 0; i < 6; i ++ )
     {
-      analogWrite ( ledPins [ i ], brightness );
+      analogWrite ( ledPins [ i ], brightnessList [ brightnessStage ] );
     }
 
     startTime = millis ();
-    delay ( 200 );
+    delay ( delayTimeList [ delayTimeStage ] );
 
     for ( int i = 0; i < 6; i ++ )
     {
